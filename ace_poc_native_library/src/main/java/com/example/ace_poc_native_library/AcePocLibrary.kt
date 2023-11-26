f8016d47f6c0ef53d3b32ce768d4a4e143a8fb89
Heena Aggarwal <heena.aggarwal@nagarro.com>
2023-11-26 12:39:58 +0530

    Initial commit

---
+++ ace_poc_native_library/src/main/java/com/example/ace_poc_native_library/AcePocLibrary.kt
@@ -0,0 +1,7 @@
+package com.example.ace_poc_native_library
+
+class AcePocLibrary {
+    fun exampleFunction(): String {
+        return "Hello from Ace poc native via aar"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ace_poc_native_via_aar"
+include(":app")
+include(":ace_poc_native_library")
